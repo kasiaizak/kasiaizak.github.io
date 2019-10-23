01598e548f2e32e65839126fb11bf39db9f07720
Kasia Izak <kontakt@kasiaizak.pl>
2019-10-23 15:49:04 +0200

    change js to ES6

--- src/js/main.js
+++ src/js/main.js
@@ -2,55 +2,56 @@
 
 if (document.body.classList.contains('landingpage-nord')) {
   console.log('ladingpage-nord');
-  var slideShow = $('.slide-show');
-  var slideCount = $('.single-slide').length;
-  var slideHeight = 100 / slideCount;
-  var slideIndex = 0;
+  const slideShow = $('.slide-show');
+  const slideCount = $('.single-slide').length;
+  let slideHeight = 100 / slideCount;
+  let slideIndex = 0;
 
   // ustawiam dynamiczną szerokość kontenera ze slajdami, zależną od ilości slajdów
   slideShow.css({
-      height: slideCount + '00%'
+    height: slideCount + '00%'
   });
 
   // ustawienie szerokości i pozycjonowanie wewnętrznych kontenerów
-  slideShow.find('.single-slide').each(function (index) {
-      $(this).css({
-          height: slideHeight + '%',
-          top: slideHeight * index + '%'
-      });
+  slideShow.find('.single-slide').each( (index) => {
+    $(this).css({
+      height: slideHeight + '%',
+      top: slideHeight * index + '%'
+    });
   });
 
   // przypisywanie zdarzeń do przycisków nawigacji
-  $('.next-slide').on('click', function () {
-      //console.log('klik naprzód');
-      slide(slideIndex + 1);
+  $('.next-slide').on('click', () => {
+    //console.log('klik naprzód');
+    slide(slideIndex + 1);
   });
 
-  $('.prev-slide').on('click', function () {
-      //console.log('klik wstecz');
-      slide(slideIndex - 1);
+  $('.prev-slide').on('click', () => {
+    //console.log('klik wstecz');
+    slide(slideIndex - 1);
   });
 
   // funkcja przesuwająca slajdy
-  function slide(newSlideIndex) {
-      if (newSlideIndex < 0 || newSlideIndex > slideCount - 1) {
-          return;
+  const slide = (newSlideIndex) => {
+    if (newSlideIndex < 0 || newSlideIndex > slideCount - 1) {
+      return;
+    }
+
+    const slideCaption = $('.slide-caption').eq(newSlideIndex);
+    const Top = newSlideIndex * -100 + '%';
+
+    slideCaption.hide();
+
+    slideShow.animate(
+      {
+        'top': Top
+      },
+      800,
+      () => {
+        slideIndex = newSlideIndex;
+        slideCaption.fadeIn();
       }
-
-      var slideCaption = $('.slide-caption').eq(newSlideIndex);
-      var Top = newSlideIndex * -100 + '%';
-
-      slideCaption.hide();
-
-      slideShow.animate({
-              'top': Top
-          },
-          800,
-          function () {
-              slideIndex = newSlideIndex;
-              slideCaption.fadeIn();
-          }
-      );
+    );
   };
 }
 
@@ -58,17 +59,17 @@ if (document.body.classList.contains('mdbootstrap-todolist')) {
   console.log('mdbootstrap-todolist');
   let position = 1;
 
-  $('#add').on('click', function() {
+  $('#add').on('click', () => {
     $('ul').append($(`<li class="list-group-item">Task number ${position}</li>`));
     position = position + 1;
   })
 
-  $('#remove').on('click', function() {
+  $('#remove').on('click', () => {
     $('li:last').remove();
     position = position - 1;
   })
 
-  $('#deleteAll').on('click', function () {
+  $('#deleteAll').on('click', () => {
     $('ul').empty();
     position = 1;
   })
